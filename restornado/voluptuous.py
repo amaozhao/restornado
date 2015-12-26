fa0e81f7f8d50ba5ae909d4af8b12b0eba776686
amaozhao <amaozhao@gmail.com>
2015-12-26 10:52:30 +0800

    update voluptuous

--- restornado/voluptuous.py
+++ restornado/voluptuous.py
@@ -1,4 +1,3 @@
-
 # encoding: utf-8
 #
 # Copyright (C) 2010-2013 Alec Thomas <alec@swapoff.org>
@@ -84,12 +83,12 @@ Validate like so:
     ...                  'Users': {'snmp_community': 'monkey'}}}}
     True
 """
+import collections
 import datetime
 import inspect
 import os
 import re
 import sys
-
 from contextlib import contextmanager
 from functools import wraps
 
@@ -100,15 +99,15 @@ if sys.version_info >= (3,):
     unicode = str
     basestring = str
     ifilter = filter
-    iteritems = dict.items
+    iteritems = lambda d: d.items()
 else:
     from itertools import ifilter
     import urlparse
-    iteritems = dict.iteritems
+    iteritems = lambda d: d.iteritems()
 
 
 __author__ = 'Alec Thomas <alec@swapoff.org>'
-__version__ = '0.8.7'
+__version__ = '0.8.8'
 
 
 @contextmanager
@@ -346,7 +345,7 @@ class Schema(object):
             return lambda _, v: v
         if isinstance(schema, Object):
             return self._compile_object(schema)
-        if isinstance(schema, dict):
+        if isinstance(schema, collections.Mapping):
             return self._compile_dict(schema)
         elif isinstance(schema, list):
             return self._compile_list(schema)
@@ -355,7 +354,7 @@ class Schema(object):
         type_ = type(schema)
         if type_ is type:
             type_ = schema
-        if type_ in (int, long, str, unicode, float, complex, object,
+        if type_ in (bool, int, long, str, unicode, float, complex, object,
                      list, dict, type(None)) or callable(schema):
             return _compile_scalar(schema)
         raise SchemaError('unsupported schema data type %r' %
